13930790408f78da6352dfcc0a7ec2c52da3780f
Nitin Sharma <108229177+Nitin-hackgramer@users.noreply.github.com>
2026-05-30 17:42:50 +0530

    initial commit

--- backend/castly/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility shim package for deployments expecting the project to be named
-"castly". This package simply ensures `uvicorn castly.asgi:application` will
-work by placing the `backend` directory on `sys.path` and loading the Django
-ASGI application from `voidx.settings`.
-"""
-
-__all__ = ["asgi"]

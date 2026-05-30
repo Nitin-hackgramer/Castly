8552567221e0ccb1561e3ac8639cdac1ec72665f
Nitin Sharma <108229177+Nitin-hackgramer@users.noreply.github.com>
2026-05-30 22:23:02 +0530

    urls.py

--- backend/voidx/urls.py
+++ backend/voidx/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/digilocker/', include('voters.urls')),
     path('api/votes/', include('votes.urls')),
     path('api/audit/', include('audit.urls')),
 ]
